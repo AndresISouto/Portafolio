05b105b4efb616bb0b69a4631671615b673e8130
AndrseISouto <186386252+AndresISouto@users.noreply.github.com>
2025-03-13 08:57:57 +0100

    Intento de añadir el pprotocolo mailto: que no me pilla el body

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ Práctica de Lenguaje de marcas: Portafolio
 
 Requisitos a forzar
 - Contacto funcional
-- cambiar los divs por articles
-- Preguntar lo del div en la imagen
 
-- media queries para moviles pequeños
 - vercel
 - Borrar el readme y poner como ejecutarlo
---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+    document.getElementById('formulario').addEventListener('submit', function(event) {
+      event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+
+      // Obtén los valores del formulario
+      const email = document.getElementById('mail').value;
+      const subject = 'Correo de ' + document.getElementById('nombre').value;
+      const message = "prueba"//document.getElementById('mensaje').value;
+
+      // Codifica los valores para que sean válidos en una URL
+      const encodedSubject = encodeURIComponent(subject);
+      const encodedMessage = encodeURIComponent(message);
+
+      // Crea el enlace mailto
+      const mailtoLink = `mailto:${email}?subject=${encodedSubject}&body=${encodedMessage}`;
+
+      // Abre el gestor de correo electrónico
+      window.location.href = mailtoLink;
+    });
+  
